92451b3b9949043cd83e960f0462a9b3b8328fce
Dusan Mesi <dusan.mesi@gmail.com>
2022-08-11 12:30:35 +0200

    dodaj, izmeni proizvod

--- StorageServiceLibrary/Dto/ProizvodDTO.cs
+++ StorageServiceLibrary/Dto/ProizvodDTO.cs
@@ -11,32 +11,40 @@ namespace StorageServiceLibrary.Dto
 {
   public  class ProizvodDTO
     {
-        public int Id { get; set; }
+         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obavezno polje, unesite ime!")]
         public string Naziv { get; set; }
+        [Required(ErrorMessage = "Obavezno polje, unesite opis!")]
         public string Opis { get; set; }
 
-
+       
         [ForeignKey("Id_kategorija")]
-        public Kategorija Kategorija { get; set; }  //ovo moze include
-        public int Id_kategorija { get; set; }
+        
+        public Kategorija Kategorija { get; set; } 
+
+        [Required]
+        [Range(1, Int32.MaxValue, ErrorMessage = "Obavezno polje, odaberite kategoriju!")]
+        public int? Id_kategorija { get; set; }
 
 
         [ForeignKey("Id_Proizvodjac")]
-        public Proizvodjac Proizvodjac { get; set; }//ovo moze include
+        public Proizvodjac Proizvodjac { get; set; }
+
+        [Required]
+        [Range(1, Int32.MaxValue, ErrorMessage = "Obavezno polje, odaberite proizvodjaca!")]
         public int Id_Proizvodjac { get; set; }
 
-        //  [ForeignKey(nameof(Dobavljac))]
+       
         [ForeignKey("Id_Dobavljac")]
-        public Dobavljac Dobavljac { get; set; }//ovo moze include
+        public Dobavljac Dobavljac { get; set; }
+        
+        [Required]
+        [Range(1, Int32.MaxValue, ErrorMessage = "Obavezno polje, odaberite dobavljaca!")]
         public int Id_Dobavljac { get; set; }
 
-
+        [Required(ErrorMessage = "Obavezno polje, unesite cenu!")]
         [Column(TypeName = "decimal(18,4)")]
         public decimal Cena { get; set; }
-
-
-        //public virtual IList<Kategorija> LKategorije { get; set; }//ovo moze include
-        //public virtual IList<Dobavljac> LDobavljaci { get; set; }//ovo moze include
-        //public virtual IList<Proizvodjac> LProizvodjaci { get; set; }//ovo moze include
     }
 }
---
+++ prakticni_zadatak_wm/Controllers/IzmeneController.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using AutoMapper;
+using StorageServiceLibrary.Dto;
+using System.IO;
+using StorageServiceLibrary.Model;
+using StorageServiceLibrary.IRepository;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace prakticni_zadatak_wm.Controllers
+{
+    public class IzmeneController : Controller
+    {
+        private readonly App_Data_Base _context;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public IzmeneController(IUnitOfWork unitOfWork, IMapper mapper, App_Data_Base context)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var proizvodi = await _unitOfWork.Proizvodi.GetAll(null, null, new List<string> { "Kategorija", "Proizvodjac", "Dobavljac" });
+
+            var results = _mapper.Map<List<ProizvodDTO>>(proizvodi);
+
+            return View(results);
+
+
+        }
+
+        public async Task<IActionResult> AddOrEdit(int? proizvodId)
+        {
+            List<SelectListItem> kategorije = _context.Kategorije
+                        .Select(k =>
+                        new SelectListItem
+                        {
+                            Value = k.Id.ToString(),
+                            Text = k.Naziv
+                        }).ToList();
+
+            ViewBag.KategorijeDDL = kategorije;
+
+            List<SelectListItem> proizvodjaci = _context.Proizvodjaci
+                   .Select(k =>
+                   new SelectListItem
+                   {
+                       Value = k.Id.ToString(),
+                       Text = k.Naziv
+                   }).ToList();
+
+            ViewBag.ProizvodjaciDDL = proizvodjaci;
+
+            List<SelectListItem> dobavljaci = _context.Dobavljaci
+                   .Select(k =>
+                   new SelectListItem
+                   {
+                       Value = k.Id.ToString(),
+                       Text = k.Naziv
+                   }).ToList();
+
+            ViewBag.DobavljaciDDL = dobavljaci;
+
+
+            ViewBag.Naslov = proizvodId == null ? "Dodaj Novi Proizvod" : "Izmeni Proizvod";
+            ViewBag.IsEdit = proizvodId == null ? false : true;
+            if (proizvodId == null)
+            {
+                return View();
+            }
+            else
+            {
+                var proizvod = await _unitOfWork.Proizvodi.Get(q => q.Id == proizvodId);
+
+                var result = _mapper.Map<ProizvodDTO>(proizvod);
+
+                return View(result);
+            }
+        }
+
+        //AddOrEdit Post Method
+        [HttpPost]
+
+        public async Task<IActionResult> AddOrEdit([FromForm] ProizvodDTO proizvod)
+        {
+            bool proizvodPostoji = false;
+
+            var proveraProizvoda = await _context.Proizvodi.FindAsync(proizvod.Id);
+
+            if (proveraProizvoda != null)
+            {
+                proizvodPostoji = true;
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //update
+                    if (proizvodPostoji)
+                    {
+                        _mapper.Map(proizvod, proveraProizvoda);
+                        _unitOfWork.Proizvodi.Update(proveraProizvoda);
+                        await _unitOfWork.Save();
+                    }
+                    else
+                    {
+                        //insert
+                        var products = _mapper.Map<Proizvod>(proizvod);
+                        await _unitOfWork.Proizvodi.Insert(products);
+                        await _unitOfWork.Save();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+    }
+}
